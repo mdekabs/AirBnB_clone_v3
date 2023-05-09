b238c7956893517c8ce88961bfe8cc5f5f622299
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-05-10 00:15:09 +0100

    made port int

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Start a flask app and register a Blueprint"""
 
+from api.v1.views import app_views
 from flask import Flask
 from flask import jsonify
-from api.v1.views import app_views
 from models import storage
 from os import getenv
 
@@ -27,4 +27,4 @@ def page_not_found(e):
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST') if getenv('HBNB_API_HOST') else '0.0.0.0'
     port = getenv('HBNB_API_PORT') if getenv('HBNB_API_PORT') else '5000'
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=host, port=int(port), threaded=True)
