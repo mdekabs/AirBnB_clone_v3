23ac3bcd43fe3e179e3f647c5f257503cc4d7cc6
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-05-10 11:48:59 +0100

    added update to city

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -64,29 +64,29 @@ def city(city_id):
 
     if not city_id:
         abort(404)
-    """Get a specific city with city_id"""
-    city = storage.get(City, city_id)
 
-    if not city:
-        abort(404)
+    else:
+        """Get a specific city with city_id"""
+        city = storage.get(City, city_id)
 
-    if request.method == 'GET':
-        return jsonify(city.to_dict())
+        if city:
+            if request.method == 'GET':
+                return jsonify(city.to_dict())
 
-    if request.method == 'DELETE':
-        storage.delete(city)
-        storage.save()
-        return jsonify({}), 200
+            if request.method == 'DELETE':
+                storage.delete(city)
+                storage.save()
+                return jsonify({}), 200
 
-    if request.method == 'PUT':
-        """Get the json data from the request body"""
-        data_json = request.get_json()
+            if request.method == 'PUT':
+                """Get the json data from the request body"""
+                data_json = request.get_json()
 
-        """Check if the data is not json"""
-        if not data_json:
-            abort(400, 'Not a JSON')
+                """Check if the data is not json"""
+                if not data_json:
+                    abort(400, 'Not a JSON')
 
-        """Update the obj with the new value"""
-        city.name = data_json.get('name')
-        city.save()
-        return jsonify(city.to_dict()), 200
+                """Update the obj with the new value"""
+                city.name = data_json.get('name')
+                city.save()
+                return jsonify(city.to_dict()), 200
