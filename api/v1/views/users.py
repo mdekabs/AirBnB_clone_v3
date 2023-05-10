de558a5d0128ece27c382b6980789abc75595afb
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-05-10 13:00:02 +0100

    updated POST in user.py RESTful

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -32,7 +32,7 @@ def users(user_id=None):
                 abort(400, 'Missing email')
             if my_dict.get("password") is None:
                 abort(400, 'Missing password')
-            new_user = user(**my_dict)
+            new_user = User(**my_dict)
             new_user.save()
             return jsonify(new_user.to_dict()), 201
     else:
